9f7849700a0c5c0be3012411f542fe2f75b004bc
celik sismanturk <redskinsrule11@yahoo.com>
2018-06-04 22:06:10 -0400

    feat: step 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// Step 3 - Add a stateful widget
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -6,7 +8,6 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -14,10 +15,22 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          //child: new Text('Hello World'), // Replace the highlighted text...
-          child: new Text(wordPair.asPascalCase),  // With this highlighted text.
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return (new Text(wordPair.asPascalCase));
   }
 }
